95ce0cff12893e9ee1022a8287f6dad133b346bf
Arshpreet Singh <73437174+GoldGroove06@users.noreply.github.com.>
2025-03-08 20:45:39 +0530

    Quiz done

--- src/app/quiz/game/page.js
+++ src/app/quiz/game/page.js
@@ -1,31 +1,125 @@
-"use client"
-import React, { useState, useEffect} from 'react';
-import { useRouter } from 'next/navigation'
+"use client";
+import React, { useState, useEffect, useRef } from "react";
+import { useRouter } from "next/navigation";
+import QuizCards from "@/components/QuizCards";
+import { IconButton, Button } from "@radix-ui/themes";
+import GameScore from "@/components/GameScore";
+import { ArrowRightIcon } from "@radix-ui/react-icons";
 
 function Game() {
-    const router = useRouter() 
-    const [score, setScore] = useState(0)
-    const handlefinish = () => {
+  const router = useRouter();
+  const [color, setColor] = useState("");
+  const [selectedValue, setSelectedValue] = useState(null);
+  const [track, setTrack] = useState(0);
+  const [key, setKey] = useState(0);
+  const [score, setScore] = useState(0);
+  const [nextBtn, setNextBtn] = useState(false);
+  const radioRef = useRef(null);
+  const gameData = [
+    {
+      question: "1. question ",
+      option1: "o1",
+      option2: "o2",
+      option3: "o3",
+      option4: "o4",
+      correct: 4,
+    },
+    {
+      question: "2. question ",
+      option1: "o1",
+      option2: "o2",
+      option3: "o3",
+      option4: "o4",
+      correct: 4,
+    },
+    {
+      question: "3. question ",
+      option1: "o1",
+      option2: "o2",
+      option3: "o3",
+      option4: "o4",
+      correct: 4,
+    },
+    {
+      question: "4. question ",
+      option1: "o1",
+      option2: "o2",
+      option3: "o3",
+      option4: "o4",
+      correct: 4,
+    },
+  ];
 
-        // send data to backend
-        localStorage.removeItem("name");
-        localStorage.removeItem("score");
-        router.push('/')
-        
+  const handlefinish = () => {
+    // send data to backend
+    localStorage.removeItem("name");
+    localStorage.removeItem("score");
+    router.push("/leaderboard");
+  };
 
+  const handleClick = (e) => {
+    const value = e.target.value;
+    setSelectedValue(value);
+    if (value == gameData[track].correct) {
+      setColor("green");
+      const curScore = score;
+      setScore(curScore + 5);
+    }
+    if (value != gameData[track].correct) {
+      setColor("red");
+      const curScore = score;
+      setScore(curScore - 3);
     }
 
-    useEffect(() => {
-        const storedScore = localStorage.getItem("score");
-        if (storedScore) setScore(storedScore);
-      }, []);
-    return (
-        <div>
-            Quiz game
-            {score}
-            <button onClick={handlefinish} >finish game</button>
-        </div>
-    );
+    setNextBtn(true);
+  };
+
+  const handleNext = () => {
+    if (track !== gameData.length - 1) {
+      setTrack(track + 1);
+      setColor("");
+
+      setSelectedValue(null);
+      const curKey = key;
+      setKey(curKey + 1);
+      setNextBtn(false);
+    }
+  };
+
+  useEffect(() => {
+    const storedScore = localStorage.getItem("score");
+    if (storedScore) setScore(storedScore);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("score", score);
+  }, [score]);
+  return (
+    <div>
+      Quiz game The Quiz Challenge is an interactive game where users answer
+      multiple-choice questions (MCQs) to test their knowledge about consumer
+      rights, BIS standards, and product safety. Current Score {score}
+      <QuizCards
+        data={gameData[track]}
+        color={color}
+        handleClick={handleClick}
+        selectedValue={selectedValue}
+        radioRef={radioRef}
+        key={key}
+      />
+      {nextBtn ? (
+        track == gameData.length - 1 ? (
+          <Button onClick={handlefinish}>finish game</Button>
+        ) : (
+          <IconButton radius="full" onClick={handleNext}>
+            <ArrowRightIcon />
+          </IconButton>
+        )
+      ) : (
+        ""
+      )}
+    </div>
+  );
 }
 
 export default Game;
---
+++ src/components/QuizCards.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { RadioCards, Flex } from "@radix-ui/themes";
+
+function QuizCards({ data, color, handleClick, selectedValue, radioRef }) {
+  return (
+    <div>
+        <Flex direction="column" gap="3" maxWidth="200px">
+      {data.question}
+      <RadioCards.Root
+        size="3"
+        color={color}
+        onClick={(e) => handleClick(e)}
+        ref={radioRef}
+      >
+        <RadioCards.Item
+          value="1"
+          disabled={selectedValue !== null && selectedValue !== "1"}
+        >
+          {data.option1}
+        </RadioCards.Item>
+        <RadioCards.Item
+          value="2"
+          disabled={selectedValue !== null && selectedValue !== "2"}
+        >
+          {data.option2}
+        </RadioCards.Item>
+        <RadioCards.Item
+          value="3"
+          disabled={selectedValue !== null && selectedValue !== "3"}
+        >
+          {data.option2}
+        </RadioCards.Item>
+        <RadioCards.Item
+          value="4"
+          disabled={selectedValue !== null && selectedValue !== "4"}
+        >
+          {data.option2}
+        </RadioCards.Item>
+      </RadioCards.Root>
+      </Flex>
+    </div>
+  );
+}
+
+export default QuizCards;
